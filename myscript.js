a6bfd1eb31ac74a90c783d8355859c86d8bb9b63
Lou Bichard <contact@louisjohnbichard.co.uk>
2023-03-25 13:39:50 +0000

    update

---
+++ myscript.js
@@ -0,0 +1,8 @@
+var script = document.createElement('script');
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-3EPLBFTB2V";
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-3EPLBFTB2V');
